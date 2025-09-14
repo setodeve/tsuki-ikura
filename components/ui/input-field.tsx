105253a6c23009d67aa73806ebccae2588653d12
setodeve <keinick11@outlook.com>
2025-09-14 12:34:46 +0900

    fix: fix style for input-field

--- components/ui/input-field.tsx
+++ components/ui/input-field.tsx
@@ -35,6 +35,7 @@ export function InputField({
           className={`
             w-full rounded-lg border px-3 py-2 text-sm
             focus:outline-none focus:ring-2
+            [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
             ${
               error
                 ? "border-red-300 focus:border-red-500 focus:ring-red-500"
